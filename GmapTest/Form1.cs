dcbd772f8c3c06442cfea45624ba4f03f9b76dd6
anton <kolesnyk.ant@gmail.com>
2020-01-23 13:43:38 +0200

    Добавьте файлы проекта.

---
+++ GmapTest/Form1.cs
@@ -0,0 +1,44 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GmapTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleSatelliteMapProvider.Instance;//.BingSatelliteMapProvider.Instance;  //BingHybridMapProvider.Instance;
+            //gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            gMapControl1.Zoom = 10;
+
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            gMapControl1.SetPositionByKeywords("Paris, France");
+            gMapControl1.ShowCenter = false;
+
+            GMapOverlay markers = new GMapOverlay("markers");
+            GMapMarker marker = new GMarkerGoogle(
+                new PointLatLng(48.8617774, 2.349272),
+                GMarkerGoogleType.blue_pushpin);
+            markers.Markers.Add(marker);
+            gMapControl1.Overlays.Add(markers);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+            gMapControl1.Refresh();
+        }
+    }
+}
